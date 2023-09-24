87c224bd245f234d26cdc3f325b27487fd064195
bianca9927 <1962163657@qq.com>
2023-09-25 00:57:49 +0800

    change avatar again

--- src/component/ChatbotAvatar.jsx
+++ src/component/ChatbotAvatar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './ChatbotAvatar.css';
 
 const BotAvatar =()=>{
-    return (<img src="https://raw.githubusercontent.com/bianca9927/Chatbot/master/src/component/service_avatar.png" width='35px' height='35px' margin-top='3px'></img>)
+    return (<img src="https://raw.githubusercontent.com/bianca9927/Chatbot/master/src/component/service_avatar.png" width='40px' height='40px' margin-top='3px'></img>)
     //<div className='prichat'>P</div>
 }
 export default BotAvatar;
